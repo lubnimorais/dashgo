7a64d542e00e3d91e9a398e7e9700d63c023d099
Lubni Morais <lubni.morais@gmail.com>
2022-03-07 15:26:29 -0300

    fix: show sidebarDrawer, input componet wrong

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -30,7 +30,7 @@ export function Sidebar() {
             <DrawerHeader>Navegação</DrawerHeader>
 
             <DrawerBody>
-              <Sidebar />
+              <SidebarNav />
             </DrawerBody>
           </DrawerContent>
         </DrawerOverlay>
